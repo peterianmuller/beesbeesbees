f2c6abd2fb4513a55c7438175066a1b840d1a1db
Peter Muller <peterianmuller@gmail.com>
2017-01-06 11:04:52 -0800

    almost done with es6 refactor

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,19 @@
 var ForagerBee = function() {
+  // Calling the bee constructor on the new instance of ForagerBee. Allows us to add methods from Bee onto foragerBee	
+  Bee.call(this);
+  this.age = 10;
+  this.job = "find pollen"
+  this.canFly = true;
+  this.treasureChest = [];
+
 };
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forage = function(treasure){
+	this.treasureChest.push(treasure);
+}
+
+
+
 
